3d5c60027e3ebc7f8ecb21700fdd1842bdcbb100
Manuel dev <mdevries@domoti.fr>
2021-11-04 15:06:58 +0100

    clean folder - TOFIX date-n get ID not last TD

--- assets/scripts/deadline_dude.js
+++ assets/scripts/deadline_dude.js
@@ -8,14 +8,14 @@ setTimeout(() => {
     var split_date = old_date.split("/");
     // Les mois 0 a 11 donc on enleve 1, cast avec *1
     var new_date = new Date(
-      split_date[2], // yy
-      split_date[1] * 1 - 1, // mm
-      split_date[0] * 1 - 14 // days
-      );
+      split_date[ 2 ], // yy
+      split_date[ 1 ] * 1 - 1, // mm
+      split_date[ 0 ] * 1 - 14 // days
+    );
 
-    var new_day   = new_date.getDate();
+    var new_day = new_date.getDate();
     var new_month = new_date.getMonth() + 1;
-    var new_year  = new_date.getYear();
+    var new_year = new_date.getYear();
 
     new_day = (new_day < 10 ? "0" : "") + new_day; // ajoute un zéro devant pour la forme
     new_month = (new_month < 10 ? "0" : "") + new_month; // ajoute un zéro devant pour la forme
@@ -24,16 +24,16 @@ setTimeout(() => {
     return new_date_text;
   }
 
-  let lib = (x) => x+6
+  let lib = (x) => x + 6
   let lib_min = 3
 
-  let ary_idbrand = ["tl-id", "al", "ho", "cd", "cv-vf", "mm", ]
-  let item = ary_idbrand.map(id => document.querySelectorAll(`#${id} + table tr td`))
+  let ary_idbrand = [ "tl-id", "al", "idh", "cd", "cv-vf", "mm", ]
+  let item = ary_idbrand.map(id => document.querySelectorAll(`#${ id } + table tr td`))
 
-  let tdBrand = item.map((tdAry,idx) => {
+  let tdBrand = item.map((tdAry, idx) => {
     // console.warn(ary_idbrand[idx], ' ',tdAry,' tdAry.length ', tdAry.length,' idbrand.length ', ary_idbrand.length)
     return {
-      "brand": ary_idbrand[idx],
+      "brand": ary_idbrand[ idx ],
       "TD": tdAry
     }
   })
@@ -41,16 +41,17 @@ setTimeout(() => {
   let objBrand = tdBrand.map(item => item)
   const redt = /[0-9]{2}\/[0-9]{2}\/[0-9]{2}/g
 
-  function setDate (brand) {
+  function setDate(brand) {
 
-    let brand_ = objBrand[brand].TD
-    // console.log(brand_)
+    let brand_ = objBrand[ brand ].TD
+    // console.log('brand_', brand_)
 
-    while (lib_min < brand_.length) {
-      var dead = brand_[lib_min].innerHTML.match(redt)
-      var dead_n = rmDaysToDate(dead[0])
+    while(lib_min < brand_.length) {
+      console.log('yo', brand_[ lib_min ].innerHTML.match(redt));
+      var dead = brand_[ lib_min ].innerHTML.match(redt)
+      var dead_n = rmDaysToDate(dead[ 0 ])
 
-      brand_[lib_min+2].innerHTML = dead_n
+      brand_[ lib_min + 2 ].innerHTML = dead_n
       lib_min = lib(lib_min)
     }
     // re init lib_min
@@ -58,8 +59,8 @@ setTimeout(() => {
   }
 
 
-    console.log('hopla?')
-    setDate(0);setDate(1);setDate(2);setDate(3);setDate(4);setDate(5);
+  console.log('hopla?')
+  setDate(0); setDate(1); setDate(2); setDate(3); setDate(4); setDate(5);
 
 }, 50);
 
